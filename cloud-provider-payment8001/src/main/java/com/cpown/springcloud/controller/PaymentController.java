924d112dd4e741677135ffe61cc07835e764eed5
chenpeng <952793966@qq.com>
2020-09-06 22:37:39 +0800

    添加服务展示，以及ip地址

--- cloud-provider-payment8001/src/main/java/com/cpown/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cpown/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.cpown.springcloud.entities.Payment;
 import com.cpown.springcloud.service.PaymentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,6 +19,11 @@ public class PaymentController {
     PaymentServiceImpl paymentService;
     @Value("${server.port}")
     private String serverPort;
+    /**
+     * 配置此信息  可以对外暴露接口展示我们系统所包含的微服务信息
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/get/{id}")
     public CommonResult get(@PathVariable("id") Long id){
@@ -39,4 +47,25 @@ public class PaymentController {
             return new CommonResult("插入数据失败,serverPort:"+serverPort,9999);
         }
     }
+    @RequestMapping("/payment/disxover")
+    public Object discover(){
+        /**
+         * 查看当前所有的微服务
+         */
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service*******"+service);
+        }
+
+        /**
+         * 获取对应微服务id 的所有实例
+         * 也就是我们的 8001  8002
+         */
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance instance : instances) {
+            log.info("---"+instance.getServiceId()+"***"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return discoveryClient;
+    }
+
 }
